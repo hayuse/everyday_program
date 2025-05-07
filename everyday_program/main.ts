d7b4af112328844e40877c2ddf15f6a031a5e782
karita <hayue0228@icloud.com>
2025-05-07 23:30:01 +0900

    https://projecteuler.net/problem=1

--- everyday_program/main.ts
+++ everyday_program/main.ts
@@ -2,8 +2,21 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', (c: any) => {
+  const targetArray = [];
+  let answer = 0;
+  for(let i = 0; i < 1000; i++) {
+    if (i % 3 === 0) {
+      targetArray.push(i);
+    }
+    if (i % 5 === 0) {
+      targetArray.push(i);
+    }
+  }
+  for (const value of targetArray) {
+    answer += value;
+  }
+  return c.text(answer);
 })
 
 Deno.serve(app.fetch)
