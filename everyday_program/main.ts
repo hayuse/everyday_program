4232aa8bbc7a38ae214bc1646aa3a6d145ce617e
karita <hayue0228@icloud.com>
2025-05-08 10:53:28 +0900

    環境の修正

--- everyday_program/main.ts
+++ everyday_program/main.ts
@@ -1,11 +1,15 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c: any) => {
+app.get("/", (c: any) => {
+  return c.text('https://projecteuler.net/ の問題を解こう！');
+});
+
+app.get("/day1", (c: any) => {
   const targetArray = [];
   let answer = 0;
-  for(let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 1000; i++) {
     if (i % 3 === 0) {
       targetArray.push(i);
       continue;
@@ -19,6 +23,35 @@ app.get('/', (c: any) => {
     answer += value;
   }
   return c.text(answer);
-})
+});
 
-Deno.serve(app.fetch)
+app.get("/day2", (c: any) => {
+  const getFebonacciArray = (init: number) => {
+    let value = init;
+    let oldValue = init;
+    const febonacchiArray = [init];
+    while (value <= 4000000) {
+      const febonacci = value + oldValue;
+      febonacchiArray.push(febonacci);
+      oldValue = value;
+      value = febonacci;
+    }
+    return febonacchiArray;
+  };
+
+  const evenSum = (array: Array<number>) => {
+    let sumValue = 0;
+    for (const value of array) {
+      if (value % 2 === 0) {
+        sumValue += value;
+      }
+    }
+    return sumValue;
+  };
+  const array = getFebonacciArray(1);
+  const answer = evenSum(array);
+
+  return c.text(answer);
+});
+
+Deno.serve(app.fetch);
