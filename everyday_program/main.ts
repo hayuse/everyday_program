332d4581bbe3032f2b4858b38ce22664a3f51144
karita <hayue0228@icloud.com>
2025-05-09 13:43:12 +0900

    ループ回数を削減

--- everyday_program/main.ts
+++ everyday_program/main.ts
@@ -58,15 +58,18 @@ app.get("/day3", (c: any) => {
   const getPrimeFactor = (init: number) => {
     let disNum = init;
     const primeFactorArray = [];
-    for (let value = 2; value <= disNum; value ++) {
-      if (disNum % value === 0) {
+    for (let value = 2; value * value <= disNum; value ++) {
+      while (disNum % value === 0) {
         primeFactorArray.push(value);
         disNum = disNum / value;
       }
     }
+    if (disNum > 1) primeFactorArray.push(disNum);
     return primeFactorArray;
   };
+
   const array = getPrimeFactor(600851475143);
+  console.log(array);
   return c.text(array[array.length - 1]);
 });
 
