18ee6b404b563635c70de7c9bc6c5718236fadcc
karita <hayue0228@icloud.com>
2025-05-15 21:23:48 +0900

    和二乗差

--- everyday_program/main.ts
+++ everyday_program/main.ts
@@ -133,4 +133,20 @@ app.get("/day5", (c: any) => {
   return c.text(main(20));
 });
 
+app.get("/day6", (c: any) => {
+  const main = (init: number) => {
+    let squareSum = 0;
+    let squareSum2 = 0;
+    for (let i = 1; i <= init; i ++) {
+      squareSum += i * i;
+    }
+    for (let i = 1; i <= init; i ++) {
+      squareSum2 += i;
+    }
+    const answer = (squareSum2 * squareSum2) - squareSum;
+    return answer;
+  }
+  return c.text(main(100));
+});
+
 Deno.serve(app.fetch);
